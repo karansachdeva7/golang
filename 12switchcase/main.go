95512d5bb0612f1349f64b35660fe3e266437520
Karan Sachdeva <karansachy@gmail.com>
2024-02-29 21:46:54 +0530

    add mod switchcase

---
+++ 12switchcase/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	fmt.Println("Switch and case in golang")
+
+	rand.Seed(time.Now().UnixNano())
+	// range is non-inclusive (you can add 1 to consider upper value)
+	diceNumber := rand.Intn(6) + 1
+
+	fmt.Println("Value of dice is ", diceNumber)
+
+	switch diceNumber {
+	case 1:
+		fmt.Println("Dice value is 1 and you can open")
+	case 2:
+		fmt.Println("You can move to 2 spot")
+	case 3:
+		// once you hit the case, all the cases need to go through n run after that
+		fmt.Println("You can move to 3 spot")
+		fallthrough
+	case 4:
+		fmt.Println("You can move to 4 spot")
+		fallthrough
+	case 5:
+		fmt.Println("You can move to 5 spot")
+	case 6:
+		fmt.Println("You can move to 6 spot and roll dice again")
+	default:
+		fmt.Println("What was that!")
+	}
+
+}
